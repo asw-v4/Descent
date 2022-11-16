14704dab518f946ee0121207d229e01a413a3aeb
Adam Ward <a.ward96@outlook.com>
2022-11-16 13:43:33 +0000

    Reorder Functions

--- app.js
+++ app.js
@@ -1,3 +1,22 @@
+function predict(input) {
+    // generate model input
+    console.log('Predicting')
+    console.log(input)
+    const inferenceInputs = input;
+    // execute the model
+    myOnnxSession.run(inferenceInputs).then((output) => {
+        // consume the output
+        const outputTensor = output.values().next().value;
+        console.log(`model output tensor: ${outputTensor.data}.`);
+    });
+};
+
+function updateInfo(content) {
+    document.getElementById('infodiv').innerHTML
+        = content;
+}
+
+
 function readURL(input) {
     if (input.files && input.files[0]) {
         var reader = new FileReader();
@@ -10,28 +29,11 @@ function readURL(input) {
         reader.readAsDataURL(input.files[0]);
         updateInfo("Identifying...")
         document.getElementsByClassName('loader')[0].style.opacity = 1;
-        console.log('ABbout to Predict')
+        console.log('Abbout to Predict')
         predict('img');
     }
 }
 
-function updateInfo(content){
-    document.getElementById('infodiv').innerHTML
-     = content ;
-}
-
-function predict(input) {
-    // generate model input
-    console.log('Predicting')
-    console.log(input)
-    const inferenceInputs = input;
-    // execute the model
-    myOnnxSession.run(inferenceInputs).then((output) => {
-      // consume the output
-      const outputTensor = output.values().next().value;
-      console.log(`model output tensor: ${outputTensor.data}.`);
-    });
-  };
 
 // create a session
 const myOnnxSession = new onnx.InferenceSession();
